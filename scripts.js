b60bfcb078719c35adf9e80b47fa9b0180326d62
Bhushan Gandla <bhushan.gandla@svclnk.com>
2023-10-22 19:22:57 -0400

    Added night support

--- scripts.js
+++ scripts.js
@@ -86,7 +86,7 @@ function bindData(weatherData){
   dataCityWeatherDescription.innerHTML = weatherData.weather[0].description;
   // dataCountryName.innerHTML = "US ";
 
-  // createWeatherMood(weatherData.weather[0].icon);
+  createWeatherMood(weatherData.weather[0].icon);
 }
 
 function createWeatherMood(weatherDescription){
@@ -99,9 +99,9 @@ function createWeatherMood(weatherDescription){
   //     break;
   //   case "03n":
   //     cityLayer.className += 'scattered-clouds';
-  //     backgroundAnimator.innerHTML = `<div class="day-cloud cloud-1"></div>
-  //     <div class="day-cloud cloud-2"></div>
-  //     <div class="day-cloud cloud-3"></div>`;
+  //     backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-1"></div>
+  //     <div class="generic-cloud cloud-2"></div>
+  //     <div class="generic-cloud cloud-3"></div>`;
   //     break;
   // }
 
@@ -116,55 +116,55 @@ function createWeatherMood(weatherDescription){
     <div class="cloud"></div>`;
   }else if(weatherDescription == '03n' || weatherDescription == '03d'){
     cityLayer.className += 'scattered-clouds';
-    backgroundAnimator.innerHTML = `<div class="day-cloud cloud-1"></div>
-    <div class="day-cloud cloud-2"></div>
-    <div class="day-cloud cloud-3"></div>`;
+    backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-1"></div>
+    <div class="generic-cloud cloud-2"></div>
+    <div class="generic-cloud cloud-3"></div>`;
   }else if(weatherDescription == '04n' || weatherDescription == '04d'){
     cityLayer.className += 'broken-clouds';
-    backgroundAnimator.innerHTML = `<div class="day-cloud cloud-1"></div>
-    <div class="day-cloud cloud-2"></div>
-    <div class="day-cloud cloud-3"></div>
-    <div class="day-cloud cloud-4"></div>
-    <div class="day-cloud cloud-5"></div>
-    <div class="day-cloud cloud-6"></div>
-    <div class="day-cloud cloud-7"></div>
-    <div class="day-cloud cloud-8"></div>`;
+    backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-1"></div>
+    <div class="generic-cloud cloud-2"></div>
+    <div class="generic-cloud cloud-3"></div>
+    <div class="generic-cloud cloud-4"></div>
+    <div class="generic-cloud cloud-5"></div>
+    <div class="generic-cloud cloud-6"></div>
+    <div class="generic-cloud cloud-7"></div>
+    <div class="generic-cloud cloud-8"></div>`;
   }else if(weatherDescription == '09n' || weatherDescription == '09d'){
     cityLayer.className += 'shower-rain';
-    backgroundAnimator.innerHTML = `<div class="day-cloud cloud-1"></div>
-    <div class="day-cloud cloud-2"></div>
-    <div class="day-cloud cloud-3"></div>
+    backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-1"></div>
+    <div class="generic-cloud cloud-2"></div>
+    <div class="generic-cloud cloud-3"></div>
     <div class="back-row-toggle">
         <div class="rain front-row"></div>
         <div class="rain back-row"></div>
     </div>`;
   }else if(weatherDescription == '10n' || weatherDescription == '10d'){
     cityLayer.className += 'rain';
-    backgroundAnimator.innerHTML = `<div class="day-cloud cloud-1"></div>
-    <div class="day-cloud cloud-2"></div>
-    <div class="day-cloud cloud-3"></div>
+    backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-1"></div>
+    <div class="generic-cloud cloud-2"></div>
+    <div class="generic-cloud cloud-3"></div>
     <div class="back-row-toggle">
         <div class="rain front-row"></div>
         <div class="rain back-row"></div>
     </div>`;
   }else if(weatherDescription == '11n' || weatherDescription == '11d'){
     cityLayer.className += 'shower-rain';
-    backgroundAnimator.innerHTML = `<div class="day-cloud cloud-1"></div>
-    <div class="day-cloud cloud-2"></div>
-    <div class="day-cloud cloud-3"></div>
+    backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-1"></div>
+    <div class="generic-cloud cloud-2"></div>
+    <div class="generic-cloud cloud-3"></div>
     <div class="thunderstorm-bg"></div>
     <div class="thunderstorms-animation"></div>`;
   }else if(weatherDescription == '13n' || weatherDescription == '13d'){
     cityLayer.className += 'snow-rain';
-    backgroundAnimator.innerHTML = `<div class="day-cloud cloud-2"></div>
+    backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-2"></div>
     <div class="snow">
         <div></div>
     </div>`;
   }else if(weatherDescription == '50n' || weatherDescription == '50d'){
     cityLayer.className += 'scattered-clouds mist';
-    backgroundAnimator.innerHTML = `<div class="day-cloud cloud-1"></div>
-    <div class="day-cloud cloud-2"></div>
-    <div class="day-cloud cloud-3"></div>
+    backgroundAnimator.innerHTML = `<div class="generic-cloud cloud-1"></div>
+    <div class="generic-cloud cloud-2"></div>
+    <div class="generic-cloud cloud-3"></div>
     <div class="mist"></div>`;
   }
 }
@@ -176,32 +176,3 @@ function addDayOrNight(icon){
     cityLayer.className += 'day ';
   }
 }
-
-
-// broken clouds
-var numberOfClouds = 9;
-let root = document.documentElement;
-function getRandomValues(min, max){
-  return (Math.random() * (max - min) + min).toFixed(0);
-}
-
-function createStylingForBrokenClouds(){
-  for(i=1; i<numberOfClouds; i++){
-    console.log(i);
-    root.style.setProperty("--cloud-width-"+i+"", ""+getRandomValues(40, 70)+"vw");
-    root.style.setProperty("--cloud-background-position-y-"+i+"", ""+getRandomValues(5, 50)+"vh");
-    root.style.setProperty("--cloud-position-"+i+"", ""+getRandomValues(0, 20)+"%");
-
-    if(i==1 || i==2 || i==3){
-      root.style.setProperty("--cloud-width-"+i+"", ""+getRandomValues(20, 50)+"vw");
-      root.style.setProperty("--cloud-background-position-y-"+i+"", ""+getRandomValues(5, 10)+"vh");
-      root.style.setProperty("--cloud-position-"+i+"", ""+getRandomValues(0, 10)+"%");
-
-      if(i==2){
-        root.style.setProperty("--cloud-position-"+i+"", ""+getRandomValues(30, 40)+"%");
-      }
-    }
-  }
-}
-
-// createStylingForBrokenClouds();
